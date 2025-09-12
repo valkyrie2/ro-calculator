a3effec62f953fdf4b7f01a1202f93b2f4b7d885
Joam <35770095+attackjom@users.noreply.github.com>
2025-09-12 21:37:15 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment: EnvironmentModel = {
   surveyUrl: 'https://forms.gle/aUYQKTfDym5Aakz9A',
   issueTrackingUrl:
     'https://docs.google.com/spreadsheets/d/1SBdqO0PzAY2rGawP_pvMAOA2erZK9_V0h2LxvcW-Epc/edit?usp=sharing',
-  roBackendUrl: 'https://prod---ro-backend-7a7aw5iyva-as.a.run.app',
+  roBackendUrl: 'https://ro-calc.luminotus.com/',
   youtubeVideoUrl: 'https://www.youtube.com/watch?v=ec5U-ZxvoFM&list=PLJi-aEFx61gw97he4dSOWP5-fADXegbyH&index=1&t',
 };
