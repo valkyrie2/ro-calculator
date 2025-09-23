1a72ec4549d71438219e3c00b9babaca74ead410
Joam <35770095+attackjom@users.noreply.github.com>
2025-09-23 19:09:25 +0700

    Add Feedback/Request Button

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnvironmentModel } from './environment.model';
 
 export const environment: EnvironmentModel = {
   production: true,
-  surveyUrl: 'https://forms.gle/aUYQKTfDym5Aakz9A',
+  surveyUrl: 'https://forms.gle/TRDtvj5MEK2gWsE29',
   issueTrackingUrl:
     'https://docs.google.com/spreadsheets/d/1SBdqO0PzAY2rGawP_pvMAOA2erZK9_V0h2LxvcW-Epc/edit?usp=sharing',
   roBackendUrl: 'https://ro-calc.luminotus.com',
