8488c50938185eb2a2a6fc43ac866f03b5b97a19
Joam <35770095+attackjom@users.noreply.github.com>
2025-09-29 01:32:55 +0700

    Add Issue Tracking

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: EnvironmentModel = {
   production: true,
   surveyUrl: 'https://forms.gle/TRDtvj5MEK2gWsE29',
   issueTrackingUrl:
-    'https://docs.google.com/spreadsheets/d/1SBdqO0PzAY2rGawP_pvMAOA2erZK9_V0h2LxvcW-Epc/edit?usp=sharing',
+    'https://docs.google.com/spreadsheets/d/1Zq9dkejS4d-kjUQ1kbSAmR0im7Hw5onfGEs7gLZ3vY4/edit?usp=sharing',
   roBackendUrl: 'https://ro-calc.luminotus.com',
   youtubeVideoUrl: 'https://www.youtube.com/watch?v=ec5U-ZxvoFM&list=PLJi-aEFx61gw97he4dSOWP5-fADXegbyH&index=1&t',
 };
